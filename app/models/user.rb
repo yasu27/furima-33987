387bb56a232d32fde539c357b75c9f12a4d626f2
yasu27 <77082468+yasu27@users.noreply.github.com>
2021-02-18 19:33:44 +0900

    修正1

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,12 +10,18 @@ class User < ApplicationRecord
 
   with_options presence: true do
     validates :nickname
-    validates :first_name, format: { with:/\A[ぁ-んァ-ン一-龥]/ }
-    validates :last_name, format: { with:/\A[ぁ-んァ-ン一-龥]/ }
-    validates :first_name_kana, format: { with:/\A[ァ-ヶー－]+\z/ }
-    validates :last_name_kana, format: { with:/\A[ァ-ヶー－]+\z/ }
     validates :birth
     validates :email, uniqueness: true
-    validates :password, length: { minimum: 6 }
+    validates :password, length: { minimum: 6 },format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{7,}/}
+  end
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ } do
+    validates :last_name
+    validates :first_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }do
+    validates :last_name_kana
+    validates :first_name_kana
   end
 end
