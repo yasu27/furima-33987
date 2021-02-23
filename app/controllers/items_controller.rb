222b899b057c86f011de5ed789d8ba2221123a20
yasu27 <77082468+yasu27@users.noreply.github.com>
2021-02-23 17:38:51 +0900

    販売手数料

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,7 +11,7 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-    if @item.save!
+    if @item.save
       redirect_to root_path
     else
       render :new
--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -5,8 +5,11 @@ window.addEventListener('load', () => {
 const priceInput = document.getElementById("item-price");
 priceInput.addEventListener("input", () => {
   const inputValue = priceInput.value;
+  const SalesCommission = Math.floor(inputValue * 0.1);
   const addTaxDom = document.getElementById("add-tax-price");
-  addTaxDom.innerHTML = Math.floor( inputValue * 0.1)// Math.floor( 195 * 0.1)
+  addTaxDom.innerHTML = SalesCommission // Math.floor( 195 * 0.1)
+  const PriceContent = document.getElementById("profit");
+  PriceContent.innerHTML = Math.floor( inputValue - SalesCommission)
 })
 });
 
