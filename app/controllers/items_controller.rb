dd90f2b7cfdde197e179aa6287ebf0c47351ecdb
yasu27 <77082468+yasu27@users.noreply.github.com>
2021-02-26 16:06:37 +0900

    修正２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -45,7 +45,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    unless user_signed_in? && current_user.id == @item.user_id
+    unless  current_user.id == @item.user_id
       redirect_to action: :index
     end
   end
